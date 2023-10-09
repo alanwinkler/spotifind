a738a8851ea08cff1bf7c4cc1bae8f18fd2793ac
maxiagui <71462236+maxiagui@users.noreply.github.com>
2023-10-09 17:04:17 -0300

    Add files via upload

---
+++ assets/js/scriptElements.js
@@ -0,0 +1,29 @@
+document.getElementById("idheader").innerHTML = `
+<div class="header-container">
+    <div class="menu-desplegable">
+                    <input type="checkbox" id="menu-btn">
+                    <label for="menu-btn" class="menu-icon">&#9776;</label>
+                    <ul class="menu">
+                        <li><a href="#">User</a></li>
+                        <li class="separator"></li>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="contact.html">About us</a></li>
+                    </ul>
+                </div>
+    <div class="div-separador"></div>
+    <div class="logoSearch-container">
+        <h1>Spotifind</h1>
+        <div>Busqueda</div>
+        <div>icono busqueda</div>
+    </div>
+    <div class="div-separador"></div>
+</div>`
+document.getElementById("idfooter").innerHTML = `<div class="footer-container"> 
+<div>
+<a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter"
+        aria-hidden="true"></i></a>
+    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"
+        aria-hidden="true"></i></a>
+    <a href="https://www.instagram.com/?hl=es" target="_blank"><i class="fa fa-instagram"
+        aria-hidden="true"></i></a>
+</div> <div>®Copyrght 2023</div> </div>`
